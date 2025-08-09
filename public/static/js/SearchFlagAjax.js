2377e56d3950fa8b8818dfa955c7f5ecf2432143
OttozaoJooj <otto.mozale007@gmail.com>
2025-08-09 11:37:57 -0300

    feat: add searchFlag ajax about search flag feature

--- public/static/js/SearchFlagAjax.js
+++ public/static/js/SearchFlagAjax.js
@@ -1,1 +1,31 @@
-console.log('Hello World');
+let iptSearchFlag = document.querySelector('#search-flag');
+let containerDataFlags = document.querySelector('#container-data-flags');
+
+
+iptSearchFlag.addEventListener('input', (e) =>{
+    let url = 'http://localhost:8000/search-flag?search-value='+e.target.value;
+    
+    fetch(url, {
+        method: 'GET'
+    })
+    .then(response => response.json())
+    .then(data => renderDataFlags(data));
+})
+
+function renderDataFlags(data){
+    containerDataFlags.innerHTML = '';
+    data.forEach(flag => {
+        containerDataFlags.innerHTML += `
+         <tr>
+            <th>${flag['id']}</th>
+            <td>${flag['country_name']}</td>
+            <td>${flag['code']}</td>
+            <td>${flag['flag_filename']}</td>
+            <td>${flag['updated_at']}</td>                    
+            <td>${flag['created_at']}</td>                                
+            <td>${flag['fk_users']}</td>                                
+        </tr> 
+        
+        `
+    });
+}
